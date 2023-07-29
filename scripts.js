492c9fd535b8069785a7be61f6bc18a58a8d4089
Lulu <112640408+HopeICanChangeMyUsername@users.noreply.github.com>
2023-07-29 09:41:50 -0300

    Add animation for restock and buy on click

--- scripts.js
+++ scripts.js
@@ -2,5 +2,11 @@ let items = document.getElementsByClassName("item");
 console.log(items);
 
 for (let element of items) {
-  element.addEventListener("click", (e) => console.log(e.target.textContent));
+  element.addEventListener("click", (e) => {
+    let currentElement = e.target;
+    let foodIcon = e.target.firstChild;
+    console.log(e.target.textContent);
+    foodIcon.classList.add("animate");
+    setTimeout(() => foodIcon.classList.remove("animate"), 6000);
+  });
 }
