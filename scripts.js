0bc24f939b72bc0cc12b021955d6b64a4f37643a
Lulu <112640408+HopeICanChangeMyUsername@users.noreply.github.com>
2023-07-30 10:41:10 -0300

    Fix pointer event not working as inteded

--- scripts.js
+++ scripts.js
@@ -5,8 +5,12 @@ for (let element of items) {
   element.addEventListener("click", (e) => {
     let currentElement = e.target;
     let foodIcon = e.target.firstChild;
+    currentElement.style.pointerEvents = "none";
     console.log(e.target.textContent);
     foodIcon.classList.add("animate");
-    let timeout = setTimeout(() => foodIcon.classList.remove("animate"), 3000);
+    let timeout = setTimeout(() => {
+      foodIcon.classList.remove("animate");
+      currentElement.style.pointerEvents = "";
+    }, 3000);
   });
 }
